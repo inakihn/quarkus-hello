d6ae743f9e809314542ca9173ccea783d401554a
Inaki Huete Nieves <inaki.huetenieves@maibornwolff.de>
2022-11-10 13:11:41 +0100

    Initial commit

---
+++ src/test/java/com/inaki/GreetingResourceTest.java
@@ -0,0 +1,22 @@
+package com.inaki;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        System.out.println("Hello");
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello from RESTEasy Reactive"));
+    }
+
+}
