cf7b2b9cf4d470787749cc5da42a06213723da5b
Inaki Huete Nieves <inaki.huetenieves@maibornwolff.de>
2022-11-17 11:28:25 +0100

    Implement Customer API as example

---
+++ src/main/java/com/inaki/customer/CustomerMapper.java
@@ -0,0 +1,12 @@
+package com.inaki.customer;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "cdi")
+public interface CustomerMapper {
+
+    CustomerEntity toEntity(Customer domain);
+
+    Customer toDomain(CustomerEntity entity);
+
+}
---
+++ src/main/java/com/inaki/customer/ServiceException.java
@@ -0,0 +1,9 @@
+package com.inaki.customer;
+
+public class ServiceException extends RuntimeException {
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+}
